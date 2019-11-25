a9410e4c2d4b4d814bdfc29c154017f2a239df56
Артём Максимович Полянский <Lazemir@yandex.ru>
2019-11-25 18:14:58 +0300

    Реализвано управление 2d. Стрелка поворачивается и бегает

--- DooM_main.py
+++ DooM_main.py
@@ -1,54 +1,101 @@
-import tkinter as tk
+import pygame
+import random
 import math
-import time
+import os
+
+WIDTH = 800
+HEIGHT = 650
+FPS = 60
+
+# Задаем цвета
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+game_folder = os.path.dirname(__file__)
+img_folder = os.path.join(game_folder, 'img')
+player_img = pygame.image.load(os.path.join(img_folder, 'player.png'))
 
 
-def main():
-	"""Главная функция главного модуля.
-	Создаёт объекты графического дизайна библиотеки tkinter: окно, холст, фрейм с кнопками, кнопки.
-	"""
-	global physical_time
-	global displayed_time
-	global time_step
-	global time_speed
-	global space
-	global start_button
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.original_image = player_img
+        self.image = self.original_image
+        self.rect = self.image.get_rect()
+        self.rect.center = (WIDTH / 2, HEIGHT / 2)
+        self.fi = 0;
 
-	print('Modelling started!')
-	physical_time = 0
+    def update(self):
+        speed_rel_x = 0
+        speed_rel_y = 0
+        w = 0
 
-	root = tkinter.Tk()
-	# космическое пространство отображается на холсте типа Canvas
-	space = tkinter.Canvas(root, width=window_width, height=window_height, bg="black")
-	space.pack(side=tkinter.TOP)
-	# нижняя панель с кнопками
-	frame = tkinter.Frame(root)
-	frame.pack(side=tkinter.BOTTOM)
+        keystate = pygame.key.get_pressed()
+        if keystate[pygame.K_LEFT]:
+            w = 5 * math.pi / 180
+        if keystate[pygame.K_RIGHT]:
+            w = -5 * math.pi / 180
+        self.fi += w
+        self.image = pygame.transform.rotate(self.original_image, int(self.fi * 180 / math.pi))
+        self.rect = self.image.get_rect(center=self.rect.center)
 
-	start_button = tkinter.Button(frame, text="Start", command=start_execution, width=6)
-	start_button.pack(side=tkinter.LEFT)
+        if keystate[pygame.K_a]:
+            speed_rel_x = -8
+        if keystate[pygame.K_d]:
+            speed_rel_x = 8
 
-	time_step = tkinter.DoubleVar()
-	time_step.set(1000)
-	time_step_entry = tkinter.Entry(frame, textvariable=time_step)
-	time_step_entry.pack(side=tkinter.LEFT)
+        if keystate[pygame.K_w]:
+            speed_rel_y = -8
+        if keystate[pygame.K_s]:
+            speed_rel_y = 8
 
-	time_speed = tkinter.DoubleVar()
-	scale = tkinter.Scale(frame, variable=time_speed, orient=tkinter.HORIZONTAL)
-	scale.pack(side=tkinter.LEFT)
+        speed_x = speed_rel_x * math.cos(self.fi) + speed_rel_y * math.sin(self.fi)
+        self.rect.x += speed_x
+        speed_y = - speed_rel_x * math.sin(self.fi) + speed_rel_y * math.cos(self.fi)
+        self.rect.y += speed_y
 
-	load_file_button = tkinter.Button(frame, text="Open file...", command=open_file_dialog)
-	load_file_button.pack(side=tkinter.LEFT)
-	save_file_button = tkinter.Button(frame, text="Save to file...", command=save_file_dialog)
-	save_file_button.pack(side=tkinter.LEFT)
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
+        if self.rect.left < 0:
+            self.rect.left = 0
+        if self.rect.bottom > HEIGHT:
+            self.rect.bottom = HEIGHT
+        if self.rect.top < 0:
+            self.rect.top = 0
 
-	displayed_time = tkinter.StringVar()
-	displayed_time.set(str(physical_time) + " seconds gone")
-	time_label = tkinter.Label(frame, textvariable=displayed_time, width=30)
-	time_label.pack(side=tkinter.RIGHT)
 
-	root.mainloop()
-	print('Modelling finished!')
 
-if __name__ == "__main__":
-	main()
+# Создаем игру и окно
+pygame.init()
+pygame.mixer.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("My Game")
+clock = pygame.time.Clock()
+all_sprites = pygame.sprite.Group()
+player = Player()
+all_sprites.add(player)
+
+# Цикл игры
+running = True
+while running:
+    # Держим цикл на правильной скорости
+    clock.tick(FPS)
+    # Ввод процесса (события)
+    for event in pygame.event.get():
+        # check for closing window
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Обновление
+    all_sprites.update()
+
+    # Рендеринг
+    screen.fill(WHITE)
+    all_sprites.draw(screen)
+    # После отрисовки всего, переворачиваем экран
+    pygame.display.flip()
+
+pygame.quit()
